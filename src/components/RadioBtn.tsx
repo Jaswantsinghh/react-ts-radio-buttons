0606a8f919c59489772dad90311fbbecedee5db1
singhjaswant <jaswant@ryaz.io>
2023-04-04 18:03:51 +0530

    Fix: accessbility and style issue

--- src/components/RadioBtn.tsx
+++ src/components/RadioBtn.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import PropTypes from 'prop-types';
 
 type AppProps = {
@@ -13,28 +13,42 @@ type AppProps = {
 
 const RadioBtn = ({ options, value, setValue }: AppProps) => {
 
-    const [isFocused, setIsFocused] = React.useState<boolean>(false);
-
     const handleChange = (option: string) => {
         if (setValue) setValue(option);
     };
 
+    const ref = useRef<HTMLDivElement>(null);
+
     const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>, option: string) => {
-        e.preventDefault();
-        if (e.key === 'Enter' || e.key === ' ') {
-            handleChange(option)
-        }
-        if (e.key === 'ArrowLeft' || e.key === 'ArrowUp') {
-            const index = options.findIndex((item) => {return item.value === option});
-            if (index > 0) {
-                handleChange(options[index - 1].value);
-            }
-        }
-        if (e.key === 'ArrowRight' || e.key === 'ArrowDown') {
-            const index = options.findIndex((item) => {return item.value === option});
-            if (index < options.length - 1) {
-                handleChange(options[index + 1].value);
-            }
+        switch (e.key) {
+            case 'Enter':
+                e.preventDefault();
+                handleChange(option);
+                break;
+            case ' ':
+                e.preventDefault();
+                handleChange(option);
+                break;
+            case 'ArrowUp':{
+                e.preventDefault();
+                const index = options.findIndex((item) => {return item.value === option});
+                if (index > 0) {
+                    handleChange(options[index - 1].value);
+                } else {
+                    handleChange(options[options.length - 1].value);
+                }
+                break;}
+            case 'ArrowDown':{
+                e.preventDefault();
+                const index2 = options.findIndex((item) => {return item.value === option});
+                if (index2 < options.length - 1) {
+                    handleChange(options[index2 + 1].value);
+                } else {
+                    handleChange(options[0].value);
+                }
+                break;}
+            default:
+                break;
         }
     };
 
@@ -42,14 +56,15 @@ const RadioBtn = ({ options, value, setValue }: AppProps) => {
     <div className="radio-btn">
         {options.map((option) => {return (
             <div 
-                role="button" 
+                role="tab" 
                 className='radio-btn__option' 
-                key={option.id} 
+                key={option.id}
+                ref={ref}
                 onClick={() => {return handleChange(option.value)}} 
                 onKeyDown={(e) => {return handleKeyDown(e, option.value)}} 
-                tabIndex={isFocused ? 0 : -1} 
-                onFocus={() => {return setIsFocused(true)}}
-                onBlur={() => {return setIsFocused(false)}}>
+                tabIndex={0}
+                aria-selected={value === option.value}
+                >
 
                     <div className="radio-btn__outer-ring">
                         <div className={`radio-btn__input ${value === option.value && 'selected'}`} />
